b30e14a52d3aa27ceb4560c0ac1123caaebd3028
aidogey <abdukarimovaid@gmail.com>
2026-08-29 12:25:33 +0500

    lesson 1: add VariableNamesTheme.java

---
+++ lesson1/VariableNamesTheme.java
@@ -0,0 +1,138 @@
+public class VariableNamesTheme {
+    public static void main(String[] args) {
+        // РАЗНЫЕ ПЕРЕМЕННЫЕ (это название блока)
+        
+        // цифра
+        int digit = 3;
+
+        // сумма чисел
+        int sumNumber = 3 + 4;
+
+        // произведение цифр
+        int productNumber = 4 * 7;
+
+        // маскимальное число
+        int maximumNumber = 100;
+
+        // количество десятков
+        int countTen = 10;
+
+        // вес собаки
+        int weightDog = 20;
+
+        // исходное число
+        int initialNumber = 1;
+
+        // символ &
+        char ampersand = '&';
+
+        // код ошибки
+        int errorCode = 501;
+
+        // тип сообщения
+        String messageType = "SMS";
+
+        // число нулей
+        int zeroCount = 0;
+
+        // уникальное число
+        int uniqueNumber = 2;
+
+        // случайное число
+        int randomNumber = 103044;
+
+        // математическое выражение
+        String mathExpression = "2 + 6 = 8";
+
+        // счет в игре
+        int gameScore = 3;
+
+        // максимальная длина
+        int maximumLength = 100;
+
+        // пункт меню
+        int menuItem = 1;
+
+        // стоимость кофе
+        int priceCoffee = 200;
+
+        // начальная дата
+        String startDate = "29.08.2026";
+
+        // окончание диапазона
+        int rangeEnd = 8;
+
+        // имя работника месяца
+        String employeeOfTheMonth = "Aidana";
+
+        // название электронной книги
+        String ebookTitle = "Circe";
+
+        // размер
+        int size = 1000000;
+
+        // вместимость 
+        int capacity = 38484;
+
+        // счетчик
+        int counter = 10;
+
+        // путь до файла
+        String path = "C:\\Users\\Aidana\\Desktop\\startjava\\lesson1\\VariableNamesTheme.java";
+
+        // количество чисел в строке
+        int numberCount = 10;
+
+        // BOOLEAN-ПЕРЕМЕННЫЕ
+
+        // сотни равны?
+        boolean areHundredsEqual = true;
+
+        // компьютер включен?
+        boolean isComputerOn = true;
+
+        // есть равные цифры?
+        boolean hasEqualNumbers = true;
+
+        // служба создана?
+        boolean isServiceCreated = false;
+
+        // файл пустой?
+        boolean isFileEmpty = false;
+
+        // соединение активное?
+        boolean isConnectionActive = false;
+
+        // новый?
+        boolean isNew = true;
+
+        // электронная почта действительная?
+        boolean isEmailValid = true;
+
+        // имеются уникальные строки?
+        boolean hasUniqueStrings = true;
+
+        // АББРЕВИАТУРЫ
+
+        // старый universally unique identifier
+        String oldUuid = "550e8400-e29b-41d4-a716-446655440000";
+
+        // производитель оперативной памяти
+        String ramManufacturer = "Intel";
+
+        // емкость жесткого диска 
+        int hddCapacity = 1024; 
+
+        // определение термина протокола передачи гипертекста 
+        String httpDefinition = "HyperText Transfer Protocol"; 
+
+        // сокращенный uniform resource locator 
+        String shortUrl = "example.com"; 
+
+        // новый идентификатор клиента 
+        String newClientId = "123456"; 
+
+        // количество символов в american standard code for information interchange 
+        int asciiCharCount = 128;
+    }
+}
